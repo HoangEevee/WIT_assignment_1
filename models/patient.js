3c1434742746170fb8f4364758e224ff542bf512
Phyllocactus <phyllocactus.essence@gmail.com>
2022-04-20 00:02:59 +1000

    glucose type

--- models/patient.js
+++ models/patient.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({ 
     patient_name: { type: String, required: true },
-    glucose: { type: Double, required: true }
+    glucose: mongoose.Decimal128
 }) 
 
 const Author = mongoose.model('Patient', schema) 
