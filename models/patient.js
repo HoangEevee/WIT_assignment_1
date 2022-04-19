12b779a9ed565f960d492eb6c13f70aa9d81289c
Phyllocactus <phyllocactus.essence@gmail.com>
2022-04-19 23:55:27 +1000

    still figuring out why data doesn't show

--- models/patient.js
+++ models/patient.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose') 
 
 const schema = new mongoose.Schema({ 
-    patient_name: { type: String, required: true }
+    patient_name: { type: String, required: true },
+    glucose: { type: Double, required: true }
 }) 
 
 const Author = mongoose.model('Patient', schema) 
