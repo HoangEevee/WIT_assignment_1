88c932cf757c953d05d21bc0ab8a7bde60a1f60c
HoangEevee <nguyenquochoang2806@gmail.com>
2022-05-16 14:46:19 +1000

    implement functional logout

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,4 +1,5 @@
 // add router
+const passport = require('passport')
 const express = require('express')
 const mainRouter = express.Router()
 
@@ -10,6 +11,10 @@ mainRouter.get("/about-diabetes",mainController.getAboutDiabetes)
 mainRouter.get("/about-website", mainController.getAboutWebsite)
 mainRouter.get("/who-are-you", mainController.getSignin)
 
+mainRouter.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
 // onwards to other routers!
 mainRouter.use('/patient', require('./patientRouter'))
 mainRouter.use('/clinician', require('./clinicianRouter'))
