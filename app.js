39452b82f601e27a6121b37a153f9b3548fae785
Phyllocactus <phyllocactus.essence@gmail.com>
2022-05-20 23:16:38 +1000

    added the other features to dashboard + highlight whether patient has entered data

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.engine(
     exphbs.engine({
         extname: 'hbs',
         helpers: {
-            outOfThreshold: (value, lower, higher) => {return value < lower || value > higher},
-            upToDate: (exists, last, today) => {return exists && (last.localeCompare(today) != 0)}
+            outOfThreshold: (value, lower, higher, exists) => {return value < lower || value > higher || (!value && exists)},
+            upToDate: (exists, last, today) => {return exists && (last.toLocaleDateString().localeCompare(today) != 0)}
         }
     })
 )
