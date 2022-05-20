5425caf0ef10ce901360669b3907664a4440fe35
EricShiInfo <67742697+EricShiInfo@users.noreply.github.com>
2022-05-21 00:27:48 +1000

    Fixing Clinician Notes and Past Health

--- routes/managePatientRouter.js
+++ routes/managePatientRouter.js
@@ -13,11 +13,14 @@ managePatientRouter.post('/new-threshold', managePatientController.setThreshold)
 managePatientRouter.get('/send-support-messages', managePatientController.getsupportmessages)
 managePatientRouter.post('/send-support-messages', managePatientController.sendSupportmessages)
 managePatientRouter.get('/view-clinical-notes', managePatientController.getClinicalNotes)
+managePatientRouter.get('/create-new-note', managePatientController.newClinicalNote)
+managePatientRouter.post('/create-new-note', managePatientController.setClinicalNote)
+managePatientRouter.get('/past-health', managePatientController.viewPatientHealth)
 
 // yet to be implemented
 // NOTE: add whatever method after the controller + add any post routes
 // These routes only follow what Quynh suggested
 //managePatientRouter.get('/support-messages', managePatientController)
 //managePatientRouter.get('/patient-info', managePatientController)
-yup
+
 module.exports = managePatientRouter
