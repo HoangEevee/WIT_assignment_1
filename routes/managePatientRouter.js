ce615db12515493e040000e31b30f7a74cf82329
Phyllocactus <phyllocactus.essence@gmail.com>
2022-05-21 01:03:37 +1000

    Revert "merge"
    This reverts commit 5e4e874494be8115881a820641b3f5aa1809f09f, reversing
    changes made to 69e7fea773614bf43da3e5da09f8f4815b3a2303.

--- routes/managePatientRouter.js
+++ routes/managePatientRouter.js
@@ -13,9 +13,6 @@ managePatientRouter.post('/new-threshold', managePatientController.setThreshold)
 managePatientRouter.get('/send-support-messages', managePatientController.getsupportmessages)
 managePatientRouter.post('/send-support-messages', managePatientController.sendSupportmessages)
 managePatientRouter.get('/view-clinical-notes', managePatientController.getClinicalNotes)
-managePatientRouter.get('/create-new-note', managePatientController.newClinicalNote)
-managePatientRouter.post('/create-new-note', managePatientController.setClinicalNote)
-managePatientRouter.get('/past-health', managePatientController.viewPatientHealth)
 
 // yet to be implemented
 // NOTE: add whatever method after the controller + add any post routes
